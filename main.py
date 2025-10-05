3948326df11816e12f5efcd548d827e5ec0d4540
Sharrmini27 <s22a0037@siswa.umk.edu.my>
2025-10-05 10:24:57 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+ import streamlit as st
+
+     st.title("SHARRMINI VEERAN")
+     st.header("0143063720")
+     st.write("s22a0037@siswa.umk.edu.my")
+     st.write("Phone: (60) 14306-3720")
+     st.write("LinkedIn: Sharrmini Veeran.com/in/yourprofile")
+
+     st.header("Education")
+     st.write("Bachelor of Information Technology, University Malaysia Kelantan, 2022")
+
+
+     st.header("Skills")
+     st.write("- Python,Java,C++")
+     st.write("- HTML, CSS, Javascript")
+
+     st.header("Projects")
+     st.write("Project Name: Description")
